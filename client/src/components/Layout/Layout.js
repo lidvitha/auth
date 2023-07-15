de1342287c136fb698b40eadc1c4a7906b96d788
lidvitha <133883115+lidvitha@users.noreply.github.com>
2023-07-15 14:36:28 +0530

    update

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -25,10 +25,10 @@ const Layout = ({ children, title, description, keywords, author }) => {
 };
 
 Layout.defaultProps = {
-  title: "Ecommerce app - shop now",
+  title: "Cfg JPMC",
   description: "mern stack project",
   keywords: "mern,react,node,mongodb",
-  author: "Techinfoyt",
+  author: "JPMC",
 };
 
 export default Layout;
